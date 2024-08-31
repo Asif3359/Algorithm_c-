1d966d8fc96e9dd9889bdaf461a9f9bc9cc2f2f0
Asif3359 <asifahammed359@gmail.com>
2024-09-01 00:37:44 +0600

    edge list use pair

---
+++ Basic_graph_BFS_DFS/stl_pair.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // pair<int, int> p;
+    // p = make_pair(10, 20);
+    pair<string, int> p;
+    p = make_pair("Asif", 20);
+
+    cout << p.first << " " << p.second << "\n";
+    return 0;
+}
